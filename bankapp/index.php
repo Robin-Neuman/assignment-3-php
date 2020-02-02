89d0a2cf7ac926e71247a6d0bdb184c1ebece468
Ironman <robinneuman@hotmail.com>
2020-02-02 01:49:23 +0100

    set up prokect

---
+++ bankapp/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bank page</title>
+</head>
+
+<body>
+    <h1>Welcome to the bank page!</h1>
+
+    <a href="">
+        <h3>Your account</h3>
+    </a>
+    <a href="">
+        <h3>Make transaction</h3>
+        <?php
+    $string = "Hello";
+    if (1 == 1) {
+print($string);
+}
+?>
+    </a>
+    
+</body>
+
+</html>
